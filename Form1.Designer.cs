d9fcd1be5d2d01c8abeefb273572bf27d49c95ac
cometHuang <sdg01821@gmail.com>
2023-06-21 18:32:45 +0800

    Add Greenmanproject

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace WindowsForm_HW3_Greenman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonstart = new System.Windows.Forms.Button();
+            this.buttonEnd = new System.Windows.Forms.Button();
+            this.labelFilename = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelSecond = new System.Windows.Forms.Label();
+            this.labelChangetime = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonstart
+            // 
+            this.buttonstart.Location = new System.Drawing.Point(37, 432);
+            this.buttonstart.Name = "buttonstart";
+            this.buttonstart.Size = new System.Drawing.Size(142, 74);
+            this.buttonstart.TabIndex = 0;
+            this.buttonstart.Text = "button1";
+            this.buttonstart.UseVisualStyleBackColor = true;
+            this.buttonstart.Click += new System.EventHandler(this.buttonstart_Click);
+            // 
+            // buttonEnd
+            // 
+            this.buttonEnd.Location = new System.Drawing.Point(233, 432);
+            this.buttonEnd.Name = "buttonEnd";
+            this.buttonEnd.Size = new System.Drawing.Size(140, 74);
+            this.buttonEnd.TabIndex = 1;
+            this.buttonEnd.Text = "button2";
+            this.buttonEnd.UseVisualStyleBackColor = true;
+            this.buttonEnd.Click += new System.EventHandler(this.buttonEnd_Click);
+            // 
+            // labelFilename
+            // 
+            this.labelFilename.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelFilename.Location = new System.Drawing.Point(406, 290);
+            this.labelFilename.Name = "labelFilename";
+            this.labelFilename.Size = new System.Drawing.Size(182, 34);
+            this.labelFilename.TabIndex = 2;
+            this.labelFilename.Text = "labelFilename";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(410, 324);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 41);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Location = new System.Drawing.Point(410, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 134);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // labelSecond
+            // 
+            this.labelSecond.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelSecond.Location = new System.Drawing.Point(406, 76);
+            this.labelSecond.Name = "labelSecond";
+            this.labelSecond.Size = new System.Drawing.Size(157, 41);
+            this.labelSecond.TabIndex = 5;
+            this.labelSecond.Text = "labelsecond";
+            // 
+            // labelChangetime
+            // 
+            this.labelChangetime.Font = new System.Drawing.Font("標楷體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelChangetime.Location = new System.Drawing.Point(406, 432);
+            this.labelChangetime.Name = "labelChangetime";
+            this.labelChangetime.Size = new System.Drawing.Size(284, 34);
+            this.labelChangetime.TabIndex = 6;
+            this.labelChangetime.Text = "labelChangetime";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(410, 466);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(178, 43);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(336, 320);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 547);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.labelChangetime);
+            this.Controls.Add(this.labelSecond);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelFilename);
+            this.Controls.Add(this.buttonEnd);
+            this.Controls.Add(this.buttonstart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonstart;
+        private System.Windows.Forms.Button buttonEnd;
+        private System.Windows.Forms.Label labelFilename;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelSecond;
+        private System.Windows.Forms.Label labelChangetime;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿//**********************************************
+//                             Name: 黃匯勛                                     //
+//                               Class: 資管三                                   //
+//                              SID: S08490027                                 //
+//                    Functions: 計時器及圖片播放                  //
+//                                   Limitations:                                   //
+//                               Assignment: No.3                             //
+//                                     Date: 5/13                                   //
+//**********************************************
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsForm_HW3_Greenman
+{
+    public partial class Form1 : Form
+    {
+        int count ,photocount;//用來判別目前的秒數與圖片張別的變數。
+        byte sec_counter = 30; //設定顯示秒數的最大值。
+        byte photo_counter = 0;//設定圖片張數的最小值。
+        byte clickevent = 0;//用來判定開始、暫停的變數。
+        int[] tickVlaue = { 1000, 900, 750 , 50, 25, 12};//每次tick秒數(毫秒/次) 前3項控制至秒數，後3項控制圖片
+        String iconFilePath = string.Empty;//設定winForm的icon路徑為空字串。
+        String greenmanFilePath = string.Empty;//設定小綠人圖片的路徑為空字串。
+        ImageList imageListBtn = new ImageList();//額外宣告一個imageList給開始、暫停以及結束的圖片用。
+                                                                              //imageList1已經有拉出來，所以沒有特別再宣告。
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DirectoryInfo greenman = new DirectoryInfo(Application.StartupPath);//用來抓取小綠人圖片之路徑。
+            greenmanFilePath = greenman.Parent.Parent.FullName + "\\image\\GreenWalking\\";
+            imageList1.ImageSize = new Size(256, 256);//設定為圖片最大值，否則會太小。
+           
+            //===============================================//
+            //   用來載入18張小綠人圖片的迴圈(圖片名稱01~18.bmp)。//
+            //===============================================//
+            for (int i = 1; i < 19; i++)//
+            {
+                if (i < 10)
+                    imageList1.Images.Add(Image.FromFile(greenmanFilePath + "0" + i.ToString() + ".bmp"));
+                else
+                    imageList1.Images.Add(Image.FromFile(greenmanFilePath + i.ToString() + ".bmp"));
+            }
+
+            //=======================================//
+            //       設定開始按鈕字串和結束按鈕字串為空       //
+            //=======================================//
+            buttonstart.Text = string.Empty;
+            buttonEnd.Text = string.Empty;
+            imageListBtn.ImageSize = new Size(42, 42);
+            
+
+            //===============================================//
+            //  抓取開始、暫停以及結束圖片進上方宣告的imageListBtn//
+            //===============================================//
+            imageListBtn.Images.Add(Image.FromFile("..\\..\\image\\PlayPauseStop\\Play.png"));
+            imageListBtn.Images.Add(Image.FromFile("..\\..\\image\\PlayPauseStop\\Pause.png"));
+            imageListBtn.Images.Add(Image.FromFile("..\\..\\image\\PlayPauseStop\\Stop.png"));
+            
+
+            //=======================================//
+            //    設定開始以及結束按鈕的字體位置和圖片      //
+            //=======================================//
+            buttonstart.BackgroundImageLayout = ImageLayout.Center;
+            buttonstart.BackgroundImage = imageListBtn.Images[0];
+            buttonEnd.BackgroundImageLayout = ImageLayout.Center;
+            buttonEnd.BackgroundImage = imageListBtn.Images[2];
+
+
+            //=======================================//
+            //           設定winForm的Icon圖片和視窗文字           //
+            //=======================================//
+            this.Icon = new Icon("..\\..\\image\\TrafficLights\\1481525071_Citycons_trafficlight.ico");
+            this.Text = "小綠人播放練習 - 變換速度";
+
+
+            //==============================================//
+            //    下方均為設定文字敘述、字體位置等一些基本設定    //
+            //==============================================//
+            labelSecond.Text = "倒數秒數 :";
+            label3.Text = "";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            label3.BackColor = Color.AliceBlue;
+            label3.ForeColor = Color.Black;
+            label3.Font = new Font("思源黑體", 40);
+            
+            
+            labelFilename.Text = "檔案名稱 :";
+            label2.Text = "";
+            label2.Font = new Font("思源黑體", 16, FontStyle.Bold);
+            label2.ForeColor = ColorTranslator.FromHtml("#3C3C3C") ; 
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            
+            
+            labelChangetime.Text = "變換時間(毫秒/次) :";
+            label6.Text = "";
+            label6.Font = new Font("思源黑體", 22, FontStyle.Bold);
+            label6.ForeColor = ColorTranslator.FromHtml("#3C3C3C");
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+
+
+            //===============================================//
+            //      設置計時器1、2的初始tickValue和紀錄值的初始值      //
+            //===============================================//
+            timer1.Interval = tickVlaue[0];
+            timer2.Interval = tickVlaue[3];
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            count = sec_counter;
+            photocount = photo_counter;
+
+        }
+
+        private void buttonEnd_Click(object sender, EventArgs e)
+        {
+            //=======================================//
+            //     按下結束按鈕時需把計時器給關閉再退出     //
+            //=======================================//
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            Application.Exit();
+        }
+
+        private void buttonstart_Click(object sender, EventArgs e)
+        {
+            //================================================//
+            //       判斷按下時的clickevent的值，用來開始以及暫停          //
+            //       首次按下clickevent會+1，之後取eventclick的餘數          //
+            //       來判斷是否繼續會暫停。                                                  //
+            //===============================================//
+
+            timer1.Interval = tickVlaue[0];//按下時同時延遲1秒
+            timer2.Interval = tickVlaue[0];
+            if (clickevent == 0)
+            {
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+                buttonstart.BackgroundImage = imageListBtn.Images[1];
+                clickevent++;
+            }
+            else if (clickevent % 2 == 1)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                buttonstart.BackgroundImage = imageListBtn.Images[0];
+                clickevent++;
+            }
+            else if (clickevent % 2 == 0)
+            {
+                timer1.Start();
+                timer2.Start();
+                buttonstart.BackgroundImage = imageListBtn.Images[1];
+                clickevent--;
+            }
+            else//clickevent的值不屬上面時會直接跳出視窗告訴錯誤。
+            {
+                MessageBox.Show("Button發生預期外的錯誤", "錯誤訊息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //================================================//
+            //       顯示倒數秒數的設定。                                                        //
+            //       當count為30時計時器設定成初始tickValue。                   //
+            //       而在設定的特定秒數(20、10秒)時修改tickValue，        //
+            //       用來加快、減慢秒數圖片的顯示以及文字描述的        //
+            //       顏色和背景顏色。                                                              //
+            //===============================================//
+            label3.Text = Convert.ToString(count);
+            
+            if (count == 30)
+            {
+                timer1.Interval = tickVlaue[0];
+                timer2.Interval = tickVlaue[3];
+                label6.Text = tickVlaue[3].ToString();
+                label3.BackColor = ColorTranslator.FromHtml("#00EC00");
+                label3.ForeColor = ColorTranslator.FromHtml("#272727");
+                count--;
+            }
+            else if (count > 20)
+            {
+                timer1.Interval = tickVlaue[0];
+                timer2.Interval = tickVlaue[3];
+                count--;
+            }
+            else if (count > 10)
+            {
+                timer1.Interval = tickVlaue[1];
+                timer2.Interval = tickVlaue[4];
+                label6.Text = tickVlaue[4].ToString();
+                label3.BackColor = ColorTranslator.FromHtml("#F9F900");
+                label3.ForeColor = ColorTranslator.FromHtml("#F75000");
+                count--;
+            }
+            else if (count > 1 && count <= 10)
+            {
+                timer1.Interval = tickVlaue[2];
+                timer2.Interval = tickVlaue[5];
+                label6.Text = tickVlaue[5].ToString();
+                label3.BackColor = ColorTranslator.FromHtml("#FF0000");
+                label3.ForeColor = ColorTranslator.FromHtml("#00FFFF");
+                count--;
+            }
+            else if (count == 1)
+            {
+                count = sec_counter;
+            }
+            else//count值不屬上面時會直接跳出視窗告訴錯誤。
+                MessageBox.Show("計時器數值內容錯誤!", "錯誤訊息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //=======================================//
+            //          按下叉叉時需把計時器給關閉再退出        //
+            //=======================================//
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            Application.Exit();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //==========================================================//
+            //        設定播放的小綠人圖片和顯示圖片時所時用的是哪一張圖片          //
+            //==========================================================//
+
+            if (photocount < 10)
+            {
+                pictureBox1.Image = imageList1.Images[photocount];
+                label2.Text = "0" + photocount + ".bmp";
+                photocount++;
+            }
+            else if (photocount >= 10 && photocount < 18)
+            {
+                pictureBox1.Image = imageList1.Images[photocount];
+                label2.Text =  photocount + ".bmp";
+                photocount++;
+            }
+            else if (photocount == 18)
+                photocount = photo_counter;
+            else//photocount值不屬上面時會直接跳出視窗告訴錯誤。
+                MessageBox.Show("計時器圖片出現錯誤!", "錯誤訊息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
